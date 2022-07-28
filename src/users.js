1d8e27590628639cf805fa618d16d8d621834bbb
001200034085a <103260092+001200034085a@users.noreply.github.com>
2022-07-28 20:55:30 +0700

    Create users.js

---
+++ src/users.js
@@ -0,0 +1,76 @@
+var express = require('express');
+const usersModel = require('../model/users.model');
+var router = express.Router();
+
+
+// post
+router.post('/', function(req, res, next) {
+    const users = new usersModel();
+    users.name=req.body.name;
+    users.age=req.body.age;
+    users.address=req.body.address;
+    users.phoneNumber=req.body.phoneNumber;
+    users.email=req.body.email;
+    users.gender=req.body.gender;
+
+    users.save((err,docs)=>{
+      if(err){
+        res.send('lỗi lưu thông tin oto')
+      }
+      else{
+        console.log('lưu thong tin thành công',docs)
+        res.send(car)
+      }
+    })
+});
+
+// get
+router.get("/",(req,res)=>{
+  const usersName=req.query.name;
+  usersModel.find({name:usersName}).exec((err,users)=>{
+    if(err){
+      res.send("không thể lấy dữ liệu")
+    }
+    else{
+      res.json(users)
+    }
+  })
+});
+// put 
+router.put("/:id",(req,res)=>{
+  usersModel.findOneAndUpdate({
+    _id:req.params.id
+  },{
+    $set:{name:req.body.name,
+      age:req.body.age,
+      address:req.body.address,
+      phoneNumber:req.body.phoneNumber,
+      email:req.body.email,
+      gender:req.body.gender},
+    
+  },{
+    upsert:true
+  },(err,users)=>{
+    if(err){
+      res.send("lỗi khi update")
+    }
+    else{
+      res.send(users);
+    }
+  })
+})
+// delete
+router.delete("/:id",(req,res)=>{
+  usersModel.findByIdAndDelete({
+    _id:req.params.id
+  },(error,users)=>{
+    if(error){
+      res.send("xảy ra lỗi khi xóa thông tin")
+    }
+    else{
+      res.send(users);
+    }
+  })
+})
+
+module.exports = router;
